9cfff5a27aab5d87515bb8b7fa3d185d20eaf01e
Remibeis <remibeis12@gmail.com>
2025-06-12 09:38:56 +0200

    css

--- src/components/ThemeSelector.jsx
+++ src/components/ThemeSelector.jsx
@@ -1,12 +1,14 @@
 import { useContext } from 'react';
 import { ThemeContext } from '../context/ThemeContext';
+import './ThemeSelector.css';
+
 
 function ThemeSelector() {
   const { theme, setTheme } = useContext(ThemeContext);
 
   return (
     <div className="theme-selector">
-      <label>Thème : </label>
+      <label>Theme : </label>
       <select value={theme} onChange={(e) => setTheme(e.target.value)}>
         <option value="PornQuiz">PornQuiz</option>
         <option value="CyberpornQuiz">CyberpornQuiz</option>
